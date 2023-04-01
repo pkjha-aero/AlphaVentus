cba4efdc4027e309692403ee5d8c0cf26db6568f
Pankaj Jha <jha3@llnl.gov>
2023-04-01 01:28:32 -0700

    Implement plotting time series and PDF of power as subplots with different choice of boundedness

--- plotting.py
+++ plotting.py
@@ -72,6 +72,54 @@ def plot_power_pdf (pickle_file_name, plot_loc, case_name, num_bins = 20, xlim =
     os.system('mkdir -p %s'%filedir)
     plt.savefig(os.path.join(filedir, filename), bbox_inches='tight')
     plt.close()
+    
+# In[]
+def plot_power_inst_pdf (pickle_file_name, plot_loc, case_name, dt, num_bins = 20, bounds_for_power = None, bounds_for_pdf = None):
+    # In[] Read the pickle data
+    with open(pickle_file_name, 'rb') as pickle_file_handle:
+        pickled_data_read = pickle.load(pickle_file_handle)
+    
+    fig, ax = plt.subplots(1, 2, sharey = True)
+    
+    # In[]: Time Series
+    time_sec = np.arange(len(pickled_data_read['power_inst']))*dt
+    time_min = time_sec/60.0
+    ax[0].plot(time_min, pickled_data_read['power_inst']/1.0e6)
+
+    ax[0].set_xlabel('Time [Min]', fontsize=14)
+    ax[0].set_ylabel('Power [MW]', fontsize=14)
+    if bounds_for_power:
+        ax[0].set_ylim([bounds_for_power[0],bounds_for_power[1]])
+    ax[0].tick_params(axis='x', labelsize=14)
+    ax[0].tick_params(axis='y', labelsize=14)
+    
+    # In[]: PDF
+    ax[1].barh(pickled_data_read['bin_centers'], pickled_data_read['hist'], color = 'lime')
+    ax[1].plot(pickled_data_read['hist'], pickled_data_read['bin_centers'], color = 'k')
+
+    ax[1].set_xlabel('PDF of power[1/MW]', fontsize=14)
+    if bounds_for_power:
+        ax[1].set_ylim([bounds_for_power[0],bounds_for_power[1]])
+    if bounds_for_pdf:
+        ax[1].set_xlim([bounds_for_pdf[0],bounds_for_pdf[1]])
+    ax[1].tick_params(axis='x', labelsize=14)
+    ax[1].tick_params(axis='y', labelsize=14)
+
+    fig.suptitle('Instantaneous power and its PDF', fontsize=14)
+    
+    # In[]: Filename
+    filename = 'Power_TS_PDF_{}_Unbounded.png'.format(case_name)
+    if bounds_for_power and not bounds_for_pdf:
+        filename = 'Power_TS_PDF_{}_Bounded_Power.png'.format(case_name)
+    if bounds_for_pdf and not bounds_for_power:
+        filename = 'Power_TS_PDF_{}_Bounded_PDF.png'.format(case_name)
+    if bounds_for_power and bounds_for_pdf:
+        filename = 'Power_TS_PDF_{}_Bounded_Power_PDF.png'.format(case_name)
+        
+    filedir = os.path.join(plot_loc, 'Instantaneous', 'Power')
+    os.system('mkdir -p %s'%filedir)
+    plt.savefig(os.path.join(filedir, filename), bbox_inches='tight')
+    plt.close()
 
 
 # In[]
