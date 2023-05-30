024c66a7fc54da5c1be347e9a0f47d0842ac1256
Pankaj Jha <jha3@llnl.gov>
2023-05-29 22:01:38 -0700

    Save power curve output in a designated directory

--- GAD_Param_Study/plot_n_cases.py
+++ GAD_Param_Study/plot_n_cases.py
@@ -19,22 +19,22 @@ import matplotlib.dates as mdates
 # Paths
 WRF_result_base_loc ='/p/lustre2/jha3/fromWill/TurbTest/NREL_5MW'
 GAD_param_output_loc = os.path.join(WRF_result_base_loc, 'GAD_Param_Study_Output')
-os.system('mkdir -p %s'%GAD_param_output_loc)
-
+GAD_param_power_curve_loc = os.path.join(GAD_param_output_loc, 'PowerCurve')
+os.system('mkdir -p %s'%GAD_param_power_curve_loc)
 #inputs
 savefig = True
 
 # Flags
-induction_effect = False
-grid_effect = True
-epsilon_effect = True
+induction_effect = True
+grid_effect = False
+epsilon_effect = False
 
 
 #wrf-gad
 # Effect of Induction Type
 if induction_effect:
-	figFileName = os.path.join(GAD_param_output_loc, 'power_induction.png')
-	tab_data_file = os.path.join(GAD_param_output_loc, 'power_induction.csv')
+	figFileName = os.path.join(GAD_param_power_curve_loc, 'power_induction.png')
+	tab_data_file = os.path.join(GAD_param_power_curve_loc, 'power_induction.csv')
 	plt_title   = '$Grid: \Delta x = \Delta y = 4 m, \Delta z = 4 m; Gaussian: \epsilon/\Delta_{grid} = 1.00 $'
 	case_dir_map = {'Glauert_Tip_dx_04_dz_04_an_fixed_at_iter_eps_1.00': {'legend':'Glauert, a$_{n}$ = 0.07, a$_{t}$ = iterated', 'line_style': 'b'},
 		        'Glauert_Tip_dx_04_dz_04_an_iter_at_iter_eps_1.00': {'legend':'Glauert, a$_{n}$ = iterated, a$_{t}$ = iterated', 'line_style': 'g'},
@@ -43,8 +43,8 @@ if induction_effect:
 			'Tip_dx_04_dz_04_an_0.0_at_0.0_eps_1.00': {'legend':'a$_{n}$ = 0.0, a$_{t}$ = 0.0', 'line_style': 'c'}}
 
 if grid_effect:
-	figFileName = os.path.join(GAD_param_output_loc, 'power_grid_effect.png')
-	tab_data_file = os.path.join(GAD_param_output_loc, 'power_grid_effect.csv')
+	figFileName = os.path.join(GAD_param_power_curve_loc, 'power_grid_effect.png')
+	tab_data_file = os.path.join(GAD_param_power_curve_loc, 'power_grid_effect.csv')
 	plt_title   = '$ Induction: Glauert, a_{n} = 0.07, a_{t} = iterated; Gaussian: \epsilon/\Delta_{grid} = 1.00 $'
 	case_dir_map = {'Glauert_Tip_dx_04_dz_04_an_fixed_at_iter_eps_1.00': {'legend':'$\Delta x = \Delta y = 4 m, \Delta z = 4 m $', 'line_style': 'b'},
 			'Glauert_Tip_dx_08_dz_08_an_fixed_at_iter_eps_1.00': {'legend':'$\Delta x = \Delta y = 8 m, \Delta z = 8 m $', 'line_style': 'g'},
@@ -53,8 +53,8 @@ if grid_effect:
 
 
 if epsilon_effect:
-	figFileName = os.path.join(GAD_param_output_loc, 'power_gaussian_effect.png')
-	tab_data_file = os.path.join(GAD_param_output_loc, 'power_gaussian_effect.csv')
+	figFileName = os.path.join(GAD_param_power_curve_loc, 'power_gaussian_effect.png')
+	tab_data_file = os.path.join(GAD_param_power_curve_loc, 'power_gaussian_effect.csv')
 	plt_title   = '$Grid: \Delta x = \Delta y = 4 m, \Delta z = 4 ; Induction: Glauert, a_{n} = 0.07, a_{t} = iterated $'
 	case_dir_map = {'Glauert_Tip_dx_04_dz_04_an_fixed_at_iter_eps_0.707': {'legend':'$ \epsilon/\Delta_{grid} = 1/\sqrt{2} $', 'line_style': 'b'},
 			'Glauert_Tip_dx_04_dz_04_an_fixed_at_iter_eps_1.00': {'legend':'$ \epsilon/\Delta_{grid} = 1.00$', 'line_style': 'g'},
@@ -147,6 +147,7 @@ tabulated_data.to_csv(tab_data_file)
 #plot
 pr = 1.00
 
+plt.cla()
 # POWER Curve
 plt.figure(1)
 plt.plot(NREL_Data['Wind Speed [m/s]'], NREL_Data['Power [kW]']*1e-3/pr, 'k-', label = 'NREL data')
@@ -157,8 +158,8 @@ for case in case_keys:
     line_style = case_dir_map[case]['line_style']
     plt.plot(vhub, power_case_ws*1e-6/pr, line_style, label=legend)
 plt.title(plt_title,fontsize=12)
-plt.xlim(0,15)
-plt.xticks(np.arange(0,21,3))
+plt.xlim(2,16)
+plt.xticks(np.arange(2,16,2))
 plt.ylim(0,7.0)
 plt.xlabel(r'Hub-height wind speed [m s$^{-1}$]',fontsize=12)
 plt.ylabel(r'Power (MW)',fontsize=12)
@@ -177,8 +178,8 @@ for case in case_keys:
     line_style = case_dir_map[case]['line_style']
     plt.plot(vhub, error_case_ws, line_style, label=legend)
 plt.title(plt_title,fontsize=12)
-plt.xlim(0,18)
-plt.xticks(np.arange(0,21,3))
+plt.xlim(2,16)
+plt.xticks(np.arange(2,16,2))
 plt.xlabel(r'Hub-height wind speed [m s$^{-1}$]',fontsize=12)
 plt.ylabel(r'Percent error w.r.t. NREL data',fontsize=12)
 plt.legend(loc='best')
